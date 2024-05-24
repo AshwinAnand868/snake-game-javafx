53aa042d4fd9e1f6822dccbd74b101bd28bcce37
AshwinAnand868 <anandashwin868@gmail.com>
2024-05-23 23:17:52 -0300

    Add the initial frame and panel classes

---
+++ src/GameFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+/**
+ * @author AshwinAnand868
+ * @version 1.0
+ * @email anandashwin868@gmail.com
+ * @since 2024-05-23
+ */
+public class GameFrame extends JFrame {
+}
---
+++ src/GamePanel.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author AshwinAnand868
+ * @version 1.0
+ * @email anandashwin868@gmail.com
+ * @since 2024-05-23
+ */
+public class GamePanel extends JPanel implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
