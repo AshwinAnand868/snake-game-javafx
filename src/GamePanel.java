1a36d94013e9f5f0196750e32887457dbbb4d658
AshwinAnand868 <anandashwin868@gmail.com>
2024-05-24 21:09:29 -0300

    Add an apple and a basic grid for testing

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
 
 /**
  * @author AshwinAnand868
@@ -10,8 +11,114 @@ import java.awt.event.ActionListener;
  */
 public class GamePanel extends JPanel implements ActionListener {
 
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+
+    // number of units in the game
+    static final int GAME_UNITS = (SCREEN_HEIGHT * SCREEN_WIDTH) / UNIT_SIZE;
+
+    // the more the delay the slower the snake
+    static final int DELAY = 75;
+
+    // body parts of the snake across x axis
+    final int[] x = new int[GAME_UNITS];
+
+    // body part of the snake across y axis
+    final int[] y = new int[GAME_UNITS];
+
+    // initial number of body parts
+    int bodyParts = 6;
+
+    int appleEaten;
+
+    // apple's x axis position
+    int appleX;
+
+    // apple's y axis position
+    int appleY;
+
+    // R represents right, L for left, U for up, D for you know now
+    char direction = 'R';
+
+    boolean running = false;
+
+    Timer timer;
+    Random random;
+
+    GamePanel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
+    }
+
+    public void startGame() {
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        // scale of 25
+        for(int i = 0; i < SCREEN_WIDTH / UNIT_SIZE; ++i) {
+            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+
+    }
+
+    public void newApple() {
+        appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+        appleY = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+    }
+
+    public void move() {
+        for(int i = bodyParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+
+        switch(direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
 
     }
 }
